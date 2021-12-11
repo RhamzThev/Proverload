ca1b37bcbad84ac9770920d8b68fa36db6ea67b2
RhamzThev <rhamzthev@gmail.com>
2021-12-11 16:16:39 -0500

    added realmdb, will work on it more tn

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import realm from './db/db';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
