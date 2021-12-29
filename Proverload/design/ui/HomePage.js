ad3c27a8ed42f1818c847c09e1a0c0195bc61353
RhamzThev <rhamzthev@gmail.com>
2021-12-29 17:02:36 -0500

    initial push for pre-alpha-workout

--- Proverload/design/ui/HomePage.js
+++ Proverload/design/ui/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button  } from 'react-native';
-
+// for workout
 function HomePage({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
