50329d03d33e090d1966627e2d310d9ecce95b85
RhamzThev <rhamzthev@gmail.com>
2022-06-14 17:04:29 -0400

    creating requirements

--- Proverload/index.js
+++ Proverload/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import UserCreation from './src/UserCreation';
+import App from './src';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => UserCreation);
+AppRegistry.registerComponent(appName, () => App);
